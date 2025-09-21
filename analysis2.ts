38480021efdc90bd325cfa90059f3943eb486876
Paul Bartlett <paul.bartlett@sony.com>
2025-09-20 22:21:45 -0700

    Missed a file

--- analysis2.ts
+++ analysis2.ts
@@ -1,5 +1,5 @@
-// Intend to get all NASDAQ stocks and then find which stocks have had the most wining streaks based on weekly data.  
-// I.e. grew each week (definition of grow need to be defined)
+// Intend to get all NASDAQ stocks and then find which stocks have had the most wining streaks based on daily data.  
+// I.e. grew each day (definition of grow need to be defined)
 
 import { Database } from "./database.js";
 import _ from "lodash";
@@ -23,31 +23,43 @@ for (const symbol of tickers) {
 
     const prices = await historyDb.collection.find({
         symbol,
-        $expr: { $eq: [{ $dayOfWeek: "$date" }, 6] },
+        // $expr: { $eq: [{ $dayOfWeek: "$date" }, 2] },
         date: { $gte: new Date(new Date().setFullYear(new Date().getFullYear() - 1)) }
     })
         .sort({ date: 1 }).toArray();
 
-    // Find the winning streaks - i.e. the number of consecutive weeks where the closing price was higher than the previous week
+    // Find the winning streaks - i.e. the number of consecutive number of days where the closing price was higher than the previous day
     let maxStreak = 0;
     let currentStreak = 0;
     let previousPrice = 0;
     let increases = 0;
     let decreases = 0;
     let unchanged = 0;
+    let bigIncrease = 0;
+    let bigDecrease = 0;
+
+    const bigChangeThreshold = 0.02; // 2%
 
     for (const price of prices) {
         // Initial case
         if (previousPrice !== 0) {
 
-            if (price.close > previousPrice) {
+            const change = (price.close - previousPrice) / previousPrice;
+
+            if (change > 0) {
                 increases++;
+                if (change > bigChangeThreshold) {
+                    bigIncrease++;
+                }
                 currentStreak++;
                 if (currentStreak > maxStreak) {
                     maxStreak = currentStreak;
                 }
-            } else if (price.close < previousPrice) {
+            } else if (change < 0) {
                 decreases++;
+                if (change < -bigChangeThreshold) {
+                    bigDecrease++;
+                }
                 currentStreak = 0;
             } else {
                 unchanged++;
@@ -59,10 +71,12 @@ for (const symbol of tickers) {
     }
 
     console.log(`Symbol: ${symbol}, Max Winning Streak: ${maxStreak}. increases: ${increases}, decreases: ${decreases}, unchanged: ${unchanged}`);
-    results.push({ symbol, maxStreak, increases, decreases, unchanged });
+    results.push({ symbol, maxStreak, increases, decreases, unchanged, bigIncrease, bigDecrease, bigDiff: bigIncrease-bigDecrease, positivity: increases === 0 ? 0 : (increases / (increases + decreases)) * 100 });
 }
 
-const sorted = _.sortBy(results, r => -r.increases);
+// const sorted = _.sortBy(results, r => -r.maxStreak);
+const filtered =  _.filter(results, r => r.increases + r.decreases > 100);
+const sorted = _.sortBy(filtered, r => -r.bigDiff);
 
 // top 10
 console.table(sorted.slice(0, 100));
